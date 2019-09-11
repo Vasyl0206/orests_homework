ca3df3a334bfa0b8bf720c54b236fdbf5bf24411
Orest Kuzymkiv <orest000@gmail.com>
2019-09-12 02:10:27 +0300

    Adding third lecture homework second task

---
+++ lect_3_2.py
@@ -0,0 +1,11 @@
+#Welcome!
+#This is my third lecture homework second part
+a= input('first number  ')
+b= input('second number  ')
+c= input('third number  ')
+d= input('fourth number  ')
+number = [int(a), int(b),int(c),int(d)]
+numberSum = sum(number)
+print('')
+print('Sum of your numbers are  ', numberSum)
+print('')
