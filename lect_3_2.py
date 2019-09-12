355204df32b7b332d8052e894f1cc3a1078e0537
Orest Kuzymkiv <orest000@gmail.com>
2019-09-12 13:57:44 +0300

    Adding third lecture homework third task of it

--- lect_3_2.py
+++ lect_3_2.py
@@ -6,8 +6,10 @@ c= input('third number  ')
 d= input('fourth number  ')
 number = [int(a), int(b),int(c),int(d)]
 numberSum = sum(number)
+#2 part
 print('')
 print('Sum of your numbers are  ', numberSum)
 print('')
 print(list(reversed(number)))
 print(sorted(number))
+#3 part
