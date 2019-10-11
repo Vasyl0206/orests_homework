6899ba56ac8b4093007d6153378269a72ef92637
Orest Kuzymkiv <orest000@gmail.com>
2019-10-12 01:13:32 +0300

    cw11.py

---
+++ cw11.py
@@ -0,0 +1,7 @@
+def areYouPlayingBanjo(name):
+    # Implement me!
+    
+    if name[0].upper() == "R":
+      return (name + " plays banjo")
+    else:
+      return (name + " does not play banjo")
